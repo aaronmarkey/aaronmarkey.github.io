1b70ebc12a1e58e72f8860e304e00383f0af4b9a
aaronmarkey <markey@hey.com>
2023-09-25 19:57:50 -0600

    * JS tests my patience every time

--- webpack.config.js
+++ webpack.config.js
@@ -20,7 +20,7 @@ module.exports = {
         loader: "babel-loader",
         options: {
           presets: ["@babel/preset-env"],
-          plugins: ["@babel/plugin-syntax-class-properties", "@babel/plugin-proposal-class-properties"]
+          plugins: ["@babel/plugin-syntax-class-properties", "@babel/plugin-transform-class-properties"]
         },
       },
       {
