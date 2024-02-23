839fbf50d42fa641bae68d2ae7e3b48caa0596e3
aaronmarkey <markey@hey.com>
2024-02-23 14:51:06 -0700

    * add ghp-import to project

--- publishconf.py
+++ publishconf.py
@@ -6,15 +6,4 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *  # noqa: F403
 
-STATIC_PATHS = [
-    "images",
-    "static",
-]
-EXTRA_PATH_METADATA = {
-    "static/favicon.ico": {"path": "favicon.ico"}
-}
-
-FEED_ALL_ATOM = "feeds/all.atom.xml"
-CATEGORY_FEED_ATOM = "feeds/{slug}.atom.xml"
-
 DELETE_OUTPUT_DIRECTORY = True
